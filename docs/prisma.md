3d8eab03bbc02d5146c1a478ab644cdfcda5eccf
Akshit Batra <akshit.batra@rea-group.com>
2024-03-04 12:57:32 +0530

    implement signup/signin functionalities

--- docs/prisma.md
+++ docs/prisma.md
@@ -16,11 +16,6 @@ Browse your data
 $ prisma studio
 ```
 
-Create migrations from your Prisma schema, apply them to the database, generate artifacts (e.g. Prisma Client)
-```shell
-$ prisma migrate dev
-```
-
 Pull the schema from an existing database, updating the Prisma schema
 ```shell
 $ prisma db pull
@@ -50,4 +45,39 @@ Display Prisma debug info
 
 ```shell
 $ prisma debug
+```
+
+## Migrations commands 
+
+Create migrations from your Prisma schema, apply them to the database, generate artifacts (e.g. Prisma Client)
+```shell
+$ prisma migrate dev
+```
+
+Reset your database and apply all migrations    
+```shell
+$ prisma migrate reset
+```
+
+Apply pending migrations to the database in production/staging
+```shell
+$ prisma migrate deploy
+```
+
+Check the status of migrations in the production/staging database
+```shell
+$ prisma migrate status
+```
+
+Specify a schema
+```shell
+$ prisma migrate status --schema=./schema.prisma
+```
+
+Compare the database schema from two databases and render the diff as a SQL script
+```shell
+$ prisma migrate diff \
+--from-url "$DATABASE_URL" \
+--to-url "postgresql://login:password@localhost:5432/db" \
+--script
 ```
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { AuthDto } from "./dto";
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
-    return { msg: 'I have signedup' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
-    return { msg: 'I have signedin' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if(!pwMatches) throw new ForbiddenException('Credentials incorrect');
+    delete user.hash;
+    return user;
   }
 }
